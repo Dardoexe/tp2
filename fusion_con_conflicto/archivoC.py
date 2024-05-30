926b72086bcc52ee4b0d9c39414909ea6d8e6727
Dardo Gonzalez <dardo135@gmail.com>
2024-05-30 20:25:31 -0300

    se modifico el archivoC.py

--- fusion_con_conflicto/archivoC.py
+++ fusion_con_conflicto/archivoC.py
@@ -1,3 +1,7 @@
+def funcionaza():
+   for i in range(45):
+     print("error")
+     
 def suma(a, b):
     print("cambio en la funcion")
     return a+b + c + f + r + t
@@ -11,3 +15,7 @@ print("La suma es:", resultado)
 
 print("La suma es:", resultao)
 print("La suma es:", reultado)
+def funcionaza():
+   for i in range(45):
+     print("error")
+    
