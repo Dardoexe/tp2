e421a5b7e9ef664a110eab56cd93d556405a8fcf
Dardo Gonzalez <dardo135@gmail.com>
2024-05-30 20:27:43 -0300

    se modifico el archivoC.py

--- fusion_con_conflicto/archivoC.py
+++ fusion_con_conflicto/archivoC.py
@@ -1,3 +1,11 @@
+class ListaAnidada:
+    def init(self, valor):
+        self.valor = valor
+        self.subelementos = []
+
+    def agregar_subelemento(self, subelemento):
+        if isinstance(subelemento, ListaAnidada):
+            self.subelementos.append(subelemento)
 def suma(a, b):
     print("cambio en la funcion")
     return a+b + c + f + r + t
